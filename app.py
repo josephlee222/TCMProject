f332c496a61018af8cc61b7e62375b1ba506e16f
Joseph Lee <facebooklee52@gmail.com>
2022-11-28 20:38:58 +0800

    Added sample code for reference

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
-
+app.secret_key = "Secret Key"
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template("home.html")
+
+
+
+@app.route('/test')
+def testpage():  # put application's code here
+    flash("This is an error alert", category="error")
+    flash("This is an info alert")
+    flash("This is a success alert", category="success")
+    return render_template("uitest.html")
+
+@app.context_processor
+def websiteContextInit():
+    return {
+        "websiteName": "TCMProject"
+    }
 
 
 if __name__ == '__main__':
