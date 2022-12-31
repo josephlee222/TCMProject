9c8e2c0aa7869d1715a219625d4a4877a8b0134c
Samuel <samuelongqixin@gmail.com>
2022-12-31 21:00:52 +0800

    created tracker class -> under tracker class there is medicine and tracker

---
+++ classes/Tracker.py
@@ -0,0 +1,45 @@
+class Tracker:
+    def __init__(self, doctor_email, duration, notes):
+        try:
+            self.doctor_email = str(doctor_email)
+            self.duration = str(duration)
+            self.notes = str(notes)
+        except ValueError:
+            print("Value error while entering medicine into Tracker class")
+
+    def getDuration(self):
+        return self.duration
+
+    def getNotes(self):
+        return self.notes
+
+    def setDuration(self,duration):
+        self.duration = duration
+
+    def setNotes(self, notes):
+        self.notes = notes
+
+class Medicine:
+
+    def __init__(self, name, description, dosage):
+        self.name = str(name)
+        self.description = str(description)
+        self.dosage = int(dosage)
+
+    def getName(self):
+        return self.name
+
+    def getDescription(self):
+        return self.description
+
+    def getDosage(self):
+        return self.dosage
+
+    def setName(self,name):
+        self.name = name
+
+    def setDescription(self,description):
+        self.description = description
+
+    def setDosage(self,dosage):
+        self.dosage = dosage
---
+++ tracker.py
@@ -0,0 +1,9 @@
+from flask import flash, Blueprint, render_template, request, session, redirect, url_for
+from functions import loginAccess
+
+tracker = Blueprint("tracker", __name__)
+
+@tracker.route("/tracker")
+@loginAccess
+def tracker():
+    print("Under construction")
