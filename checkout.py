061275322bea42005ea378eaa1e55b8dc2ba1d08
Iqram <myriq23@gmail.com>
2023-01-12 15:19:07 +0800

    Check out

--- checkout.py
+++ checkout.py
@@ -34,6 +34,8 @@ def payment():
     except Exception as e:
         return jsonify(error=str(e)), 403
 
+    print(request.get(your_url).content)
+
     return render_template("/payment/checkout.html", form=form)
 
 
