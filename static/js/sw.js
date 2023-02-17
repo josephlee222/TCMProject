6251b82d82e45bebfd6ae5db1785f62383ecca30
Joseph Lee <facebooklee52@gmail.com>
2023-02-17 17:29:57 +0800

    Fixed caching methods for PWA

--- static/js/sw.js
+++ static/js/sw.js
@@ -33,28 +33,20 @@ self.addEventListener('activate', event => {
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-    // Skip cross-origin requests, like those for Google Analytics.
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return caches.open(RUNTIME).then(cache => {
-                    return fetch(event.request).then(response => {
-                        // Put a copy of the response in the runtime cache.
-                        if (event.request.method == "GET") {
-                            return cache.put(event.request, response.clone()).then(() => {
-                                return response;
-                            });
-                        } else {
-                            return response
-                        }
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request).then(function (response) {
+            return caches.open(RUNTIME).then(cache => {
+                if (event.request.method === "GET") {
+                    return cache.put(event.request, response.clone()).then(() => {
+                        return response;
                     });
-                });
+                } else {
+                    return response
+                }
             })
-        );
-    }
-});
+        }).catch(function () {
+            return caches.match(event.request)
+        })
+    )
+})
